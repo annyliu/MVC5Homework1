d602379698d42f7f222c4d26943b1475b325ecbc
annyliu <shinasuka99@live.com>
2016-03-06 23:22:43 +0800

    顯示三資料表資料

---
+++ WebApplication3/Controllers/CustomerDataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CustomerDataController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: CustomerData
+        public ActionResult Index()
+        {
+            //return View(db.客戶資料.ToList());
+            return View();
+        }
+    }
+}
